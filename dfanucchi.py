5528537b5aad2c2f0acd741d30d707e696ab8500
Dominic Fanucchi <dfanucchi@odin.cs.csubak.edu>
2021-11-01 23:40:15 -0700

    added ability to read csv files

--- dfanucchi.py
+++ dfanucchi.py
@@ -1,3 +1,12 @@
+# COURSE: CMPS 3500
+# CLASS Project
+# PYTHON IMPLEMENTATION OF A CUSTOM STATISTICS SUMMARY CALCULATOR
+# DATE: 11/01/2021
+# STUDENT 1: Dominic Fanucchi
+# DESCRIPTION: Personal code file for Statistics Summary Calculator
+
+
+# test function for file importing
 def say_hello():
 	print('Hello, this is the beginning of a Statistics Summary Calculator\n')
 	print('For CSUB CMPS 3500 Programming Languages!')
--- statistics_summary_calculator.py
+++ statistics_summary_calculator.py
@@ -1,5 +1,52 @@
+# COURSE: CMPS 3500
+# CLASS Project
+# PYTHON IMPLEMENTATION OF A CUSTOM STATISTICS SUMMARY CALCULATOR
+# DATE: 11/01/2021
+# STUDENT 1: Dominic Fanucchi
+# STUDENT 2: 
+# STUDENT 3: 
+# DESCRIPTION: IMPLEMENTATION OF A STATISTICS SUMMARY CALCULATOR
+
 # file imports
 import dfanucchi as df
+
+# importing csv module
+import csv
+
+# naming csv files
+rideshare = "Boston_Lyft_Uber_Data.csv"
+sample = "InputDataSample.csv"
+
+# initializing the titles and rows list
+fields = []
+rows = []
+
+# reading csv files
+with open(sample, 'r') as csvfile:
+	# creating a csv reader object
+	csvreader = csv.reader(csvfile)
+
+	# extracting field names through first row
+	fields = next(csvreader)
+
+	# extracting each data row one by one
+	for row in csvreader:
+		rows.append(row)
+
+	# get total number of rows
+	print("Total number of rows: %d"%(csvreader.line_num))
+
+# printing the field names
+print('Field names are: ' + ', '.join(field for field in fields))
+
+# printing first 5 rows
+print('\nFirst 5 rows are:\n')
+for row in rows[:5]:
+	# parsing each column of a row
+	for col in row:
+		print("%10s"%col),
+	print('\n')
+
 
 df.say_hello()
 
